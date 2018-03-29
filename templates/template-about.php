51104ee7a64a76cc8f9303920bd777d934dc22c8
macteever <contact@nicolasmetivier.fr>
2018-03-29 13:36:34 +0200

    fin responsive page about

--- templates/template-about.php
+++ templates/template-about.php
@@ -4,18 +4,17 @@
       </div>
 		<div class="container-fluid about-container2">
          <div class="container">
-            <div class="row pt-150 pb-100 align-items-end mb-30">
+            <div class="row about-row pt-150 pb-100 align-items-end mx-auto mb-30">
                <?php
                   if( have_rows('about_content') ):
                       while ( have_rows('about_content') ) : the_row();
                       ?>
-                      <div class="col-xl-1 col-lg-1 col-md-1 col-12"></div>
-                       <div class="col-xl-5 col-lg-5 col-md-5 col-12">
+                       <div class="col-xl-5 col-lg-5 col-md-6 col-12 ml-auto">
                           <img class="" src="<?php the_sub_field('img'); ?>"/>
                        </div>
-                       <div class="col-xl-5 col-lg-5 col-md-5 col-12">
+                       <div class="col-xl-5 col-lg-5 col-md-6 col-12 mr-auto">
                          <h1 class="roboto-slab fs-72 ls-1 fw-700 mb-30"><?php the_sub_field('title'); ?></h1>
-                         <div class="ubuntu fs-18 lh-24 mb-30">
+                         <div class="ubuntu fs-17 lh-24 mb-30">
                             <?php the_sub_field('content'); ?>
                          </div>
                          <div class="about-link">
